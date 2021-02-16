001985fcd29c4faaefc546d961762ba80aae59f3
Tolkien Joh <jsjjsmith888@gmail.com>
2021-02-16 22:26:20 +0900

    add README.md
    Signed-off-by: Tolkien Joh <jsjjsmith888@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# Graph89-free
+
+Graph89 is an emulator targeting the Android platform for TI89, TI89T, TI92, TI92+, V200, TI84+, TI84+SE, TI83, TI83+ and TI83+SE calculators.
+
+this code is clone from Dritan Hashorva's code.
+(https://bitbucket.org/dhashoandroid/graph89-free.git)
+
+In Feb 15, 2021, 
+I just imported his code in Android Studio 4.1.2
---
+++ app/build/generated/source/buildConfig/debug/com/Bisha/TI89Emu/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.Bisha.TI89Emu;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.Bisha.TI89Emu";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1133;
+  public static final String VERSION_NAME = "1.1.3c";
+}
